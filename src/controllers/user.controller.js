b9f4244f3a2160ee66a52ca48dd12c035e84a08e
Ashish Jadhav <ashishjadhav6923@gmail.com>
2025-01-10 13:39:47 +0530

    login route is done with cookies for jwt tokens

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import apiResponse from "../utils/apiResponse.js";
 import { User } from "../models/user.model.js";
 import apiError from "../utils/apiError.js";
+import generateAccessAndRefereshTokens from "../utils/generateJwtTokens.js";
 
 const userIndexController = asyncHandler((req, res) => {
   res.status(200).json(new apiResponse(200, "This is user index route"));
@@ -44,4 +45,44 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new apiResponse(200, "User registered successfully"));
 });
 
-export { userIndexController, registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new apiError(422, "Email and Password are required");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new apiError(404, "User does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new apiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+    user._id
+  );
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const isProduction = process.env.NODE_ENV === "production";
+  const options = {
+    httpOnly: true,
+    secure: isProduction, // Use secure cookies only in production
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new apiResponse(200, "User logged In Successfully", {
+        user: loggedInUser,
+      })
+    );
+});
+
+export { userIndexController, registerUser, loginUser };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registerUser, userIndexController } from "../controllers/user.controller.js";
+import { loginUser, registerUser, userIndexController } from "../controllers/user.controller.js";
 const userRouter = Router();
 
 userRouter.route("/").get(userIndexController);
 userRouter.route("/register").post(registerUser);
+userRouter.route("/login").post(loginUser);
 
 export default userRouter;
