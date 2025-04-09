c379b6ecedb03ee6a424c33f37163ba06af29689
Ashish Jadhav <ashishjadhav6923@gmail.com>
2025-04-09 16:39:26 +0530

    minor fix

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refresh_AccessToken, verifyToken } from "../controllers/auth.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  refresh_AccessToken,
+  verifyToken,
+} from "../controllers/auth.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 const authRouter = Router();
 
@@ -8,6 +13,6 @@ authRouter.route("/login").post(loginUser);
 //verified routes
 authRouter.route("/logout").post(verifyJwt, logoutUser);
 authRouter.route("/refresh_AccessToken").post(refresh_AccessToken);
-authRouter.route("/verify").get(verifyToken);
+authRouter.route("/verify").get(verifyJwt, verifyToken);
 
 export default authRouter;
