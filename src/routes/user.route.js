961ff10450dbd5a6e869543b4bf543f5ed3363b2
Ashish Jadhav <ashishjadhav6923@gmail.com>
2025-04-09 22:41:06 +0530

    minor fix in user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -11,6 +11,6 @@ const userRouter = Router();
 userRouter.route("/info/:prn").get(getUserInfoByPrn);
 userRouter.route("/chats").get(verifyJwt, getAllUserChats);
 userRouter.route("/chat/get/:chatSessionId").get(verifyJwt, getChatMessages);
-userRouter.route("/chat/createChatSession").get(verifyJwt, createChatSession);
+userRouter.route("/chat/createChatSession").post(verifyJwt, createChatSession);
 
 export default userRouter;
